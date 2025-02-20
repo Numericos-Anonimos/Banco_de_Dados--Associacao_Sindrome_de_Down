ef0ececa00e85a64336226d5e964276b8e8f52c7
Th0m4sma <thomaspirescorreia@gmail.com>
2025-02-20 15:45:37 -0300

    Finalização_Oficinas

--- pages/Oficinas.py
+++ pages/Oficinas.py
@@ -379,11 +379,107 @@ from datetime import datetime, time
 import streamlit as st
 
 def apresentar_oficinas2(oficinas):
-    # Verificando se a oficina selecionada já foi salva no estado da sessão
-    if "oficina_selecionada" in st.session_state and st.session_state["oficina_selecionada"]:
+    if 'oficina_selecionada' in st.session_state and st.session_state['oficina_selecionada']:
         oficina_selecionada = st.session_state["oficina_selecionada"]
+    
+        # Verifique o tipo do objeto antes de tentar fazer a cópia
+        if isinstance(oficina_selecionada, dict):
+            dados = oficina_selecionada.copy()  # Só chama copy() se for um dicionário
+        else:
+            st.error("A oficina selecionada não é um dicionário!")
+            return  # Encerra a execução da função ou faz outra ação apropriada
         
-        st.title(f"Oficinas Selecionadas: {oficina_selecionada}")
+        edit_mode = st.session_state.get('edit_mode', False)
+        
+        if not st.session_state.edit_mode:
+            cols = st.columns([2.5, 1])
+            cols[0].title(f'{dados["Nome"]}')
+            cols[1].header(f'Status: {"✅" if dados["Data_Fim"] is None else "❌"}')
+            st.markdown("<br>", unsafe_allow_html=True)
+
+            cols = st.columns(3)
+            cols[0].write(f'**Categoria**: {dados["Categoria"]}')
+            cols[1].write(f'**Projeto**: {dados["Projeto"]}')
+            cols[2].write(f'**Data de Cadastro**: {dados["Data_Cadastro"].strftime("%d/%m/%Y")}{"" if dados["Data_Fim"] is None else f" - {dados['Data_Fim'].strftime('%d/%m/%Y')}" }')
+
+            cols = st.columns(3)
+            cols[0].write(f'**Dia da Semana**: {dados["Dia_Semana"]}')
+            cols[1].write(f'**Horário**: {dados["Horario_Inicio"].strftime("%H:%M")} às {dados["Horario_Fim"].strftime("%H:%M")} ({sub_hora(dados["Horario_Inicio"], dados["Horario_Fim"])} minutos)')
+            cols[2].write(f'**Usuários**: {len(dados["Participantes"])} / {dados["Max_Participantes"]}')
+
+            cols = st.columns(3)
+            cols[0].write(f'**Oficineiro**: {dados["Oficineiro"]}')
+            cols[1].write(f'**Valor por Hora**: R$ {dados["Valor_Hora"]:.2f}')
+            cols[2].write(f'**Total**: R$ {len(dados["Houve_Oficina"]) * sub_hora(dados["Horario_Inicio"], dados["Horario_Fim"]) * dados["Valor_Hora"] / 60:.2f}')
+            
+            st.write(f'**Descrição**: {dados["Descricao"]}')
+
+            if st.button("Editar"):
+                st.session_state.edit_mode = True
+                st.rerun()
+
+            st.markdown("---")
+            st.header("Presenças")
+            min_date, max_date = min(dados['Houve_Oficina']), max(dados['Houve_Oficina'])
+            cols = st.columns(2)
+
+            selected_dates = [cols[0].date_input("Data Inicial", min_date, min_value=min_date, max_value=max_date, format="DD/MM/YYYY")]
+            selected_dates.append(cols[1].date_input("Data Final", max_date, min_value=selected_dates[0], max_value=max_date, format="DD/MM/YYYY"))
+
+            show_all_dates = st.checkbox("Mostrar todas as datas")
+
+            if show_all_dates:
+                all_dates = pd.date_range(selected_dates[0], selected_dates[1])
+                date_list = [d.date() for d in all_dates]
+            else:
+                date_list = [d for d in dados['Houve_Oficina'] if selected_dates[0] <= d <= selected_dates[1]]
+
+            date_strings = [d.strftime("%d/%m/%Y") for d in date_list]
+            df = pd.DataFrame(index=dados['Participantes'].keys(), columns=date_strings, dtype=str)
+
+            for participante, presencas in dados['Participantes'].items():
+                for data in date_list:
+                    data_str = data.strftime("%d/%m/%Y")
+                    df.loc[participante, data_str] = "✅" if data in presencas else "❌" if data in dados['Houve_Oficina'] else "⬜"
+
+            st.dataframe(df, use_container_width=True)
+
+            st.markdown("---")
+            st.subheader("Fotos")
+            if st.checkbox("Mostrar Fotos"):
+                cols = st.columns(3)
+                for idx, foto in enumerate(dados['Fotos']):
+                    if os.path.exists(foto):
+                        cols[idx % 3].image(foto, use_column_width=True)
+                    else:
+                        st.error(f"Imagem não encontrada: {foto}")
+            if st.button("Voltar", key=f"voltar_{dados['Cod_Oficina']}"):
+                st.session_state["oficina_selecionada"] = None
+                st.session_state["oficina_nome_selecionada"] = None
+                st.rerun()
+
+        else:
+            novos_dados = dados.copy()
+            novos_dados["Nome"] = st.text_input("Nome", value=dados["Nome"])
+            novos_dados["Categoria"] = st.text_input("Categoria", value=dados["Categoria"])
+            novos_dados["Max_Participantes"] = st.number_input("Máximo de Participantes", value=dados["Max_Participantes"], min_value=1, max_value=100)
+            novos_dados["Valor_Hora"] = st.number_input("Valor por Hora", value=dados["Valor_Hora"], min_value=0.01, format="%.2f", step=1.0)
+            novos_dados["Descricao"] = st.text_area("Descrição", value=dados["Descricao"])
+            
+            cols = st.columns(2)
+            if cols[0].button("Salvar", use_container_width=True):
+                dados.update(novos_dados)
+                st.session_state.edit_mode = False
+                st.rerun()
+            if cols[1].button("Cancelar", use_container_width=True):
+                st.session_state.edit_mode = False
+                st.rerun()
+            st.info("Modo de edição ativado. Altere os campos e clique em 'Salvar alterações'.")
+
+    elif "oficina_nome_selecionado" in st.session_state and st.session_state["oficina_nome_selecionado"]:
+        oficina_nome_selecionado = st.session_state["oficina_nome_selecionado"]
+        
+        st.title(f"Oficinas Selecionadas: {oficina_nome_selecionado}")
         st.markdown("<br>", unsafe_allow_html=True)
 
         # Filtro por dias da semana dentro do primeiro if, usando multiselect
@@ -392,7 +488,7 @@ def apresentar_oficinas2(oficinas):
 
         st.markdown("---")
         # Filtrando oficinas com o nome igual ao selecionado
-        oficinas_filtradas = [oficina for oficina in oficinas if oficina["Nome"] == oficina_selecionada]
+        oficinas_filtradas = [oficina for oficina in oficinas if oficina["Nome"] == oficina_nome_selecionado]
 
         # Se houver dias selecionados, aplica o filtro
         if dias_selecionados:
@@ -419,17 +515,32 @@ def apresentar_oficinas2(oficinas):
                     st.markdown(f"**Data:** {data_inicio} ({dia_semana_inicio})")
                 
                 with cols[1]:
-                    if st.button("Ver Mais", key=f"ver_mais_{oficina['Cod_Oficina']}"):
-                        # Exibe mais informações quando o botão "Ver Mais" for clicado
-                        st.markdown(f"**Descrição Completa:** {oficina['Descricao']}")
-                        st.markdown(f"**Data de Cadastro:** {oficina['Data_Cadastro']}")
-                        st.markdown("---")  # Separador entre as oficinas para visualização
+                    st.markdown("""
+                        <style>
+                            .button-container {
+                                display: flex;
+                                flex-direction: column;
+                                justify-content: flex-end;
+                                height: 100%;  /* Garantir que o container ocupe toda a altura */
+                                margin-top: auto;  /* Posiciona o botão na parte inferior */
+                            }
+                        </style>
+                    """, unsafe_allow_html=True)
+
+                    with st.container():
+                        st.markdown('<div class="button-container">', unsafe_allow_html=True)
+                        if st.button("Ver Mais", key=f"ver_mais_{oficina['Cod_Oficina']}"):
+                            st.session_state["oficina_selecionada"] = oficina
+                            st.rerun()
+                        st.markdown('</div>', unsafe_allow_html=True)
+                
+                st.markdown("---")
 
         
         # Botão de voltar para a lista de oficinas
         if st.button("Voltar"):
             # Limpa o estado da oficina selecionada
-            st.session_state["oficina_selecionada"] = None
+            st.session_state["oficina_nome_selecionado"] = None
             st.rerun()  # Reinicia a página para exibir a lista inicial de oficinas
     else:
         st.title("Nossas Oficinas")
@@ -463,7 +574,7 @@ def apresentar_oficinas2(oficinas):
                 with cols[j]:
                     with st.container(border=True):
                         if st.button(oficina["Nome"], use_container_width=True, key=f"btn_{oficina['Cod_Oficina']}"):
-                            st.session_state["oficina_selecionada"] = oficina["Nome"]
+                            st.session_state["oficina_nome_selecionado"] = oficina["Nome"]
                             st.rerun()
                         st.markdown(f'<div style="text-align: center; font-size: 12px; color: gray;">{oficina["Descricao"]}</div>', unsafe_allow_html=True)
 
