4ccec6fe8888f0edbc5f0f2596ac335e59773a03
mr-miguelrangel <87578259+mr-miguelrangel@users.noreply.github.com>
2025-02-20 19:16:31 -0300

    Consultas atendidos

--- Banco_de_Dados/Atendidos.py
+++ Banco_de_Dados/Atendidos.py
@@ -47,6 +47,58 @@ def atendido_contatos (Cod_Atendido):
 
     return telefone_list
 
-#print (atendido_contatos(3))
-#print (listar_atendidos())
-#print (atendido_presencas(2))
+def atendidos_oficina (Cod_Atendido):
+    with engine.connect() as con:
+        query = sqlalchemy.text("""
+            SELECT o.Dia_Semana, o.Hora_Inicio, o.Hora_Fim, o.Nome
+            FROM Oficinas o
+            JOIN Atendido_Oficinas A ON A.Cod_Oficina = o.Cod_Oficina
+            WHERE A.Cod_Atendido = :cod;
+        """)
+
+        oficinas = con.execute(query, {"cod": Cod_Atendido})
+
+        # Mapeamento de números para dias da semana
+        dias_semana = {
+            1: "SEG", 2: "TER", 3: "QUA", 4: "QUI", 5: "SEX", 6: "SÁB", 7: "DOM"
+        }
+
+    
+        oficina_dict = {
+            f"{dias_semana.get(row[0], 'DIA INVÁLIDO')} - {row[1]} ÀS {row[2]}": row[3]
+            for row in oficinas
+        }
+
+    return oficina_dict
+
+def atendido_foto(Cod_Atendido):
+    with engine.connect() as con:
+        query = sqlalchemy.text("""
+            SELECT f.Caminho
+            FROM Fotos f
+            JOIN Atendido_Fotos A ON A.Cod_Foto = f.Cod_Foto
+            WHERE A.Cod_Atendido = :cod;
+        """)
+
+        fotos = con.execute(query, {"cod": Cod_Atendido})
+
+        
+        fotos =  [row[0] for row in fotos]
+
+    return fotos
+
+def atendido_eventos (Cod_Atendido):
+    with engine.connect() as con:
+        query = sqlalchemy.text("""
+            SELECT e.nome, e.data, e.Observacoes
+            FROM Eventos e
+            JOIN Atendido_Eventos A ON A.Cod_Evento = e.Cod_Evento
+            WHERE A.Cod_Atendido = :cod;
+        """)
+
+        eventos = con.execute(query, {"cod": Cod_Atendido})
+
+        # Retornando uma lista de tuplas (Telefone, Descrição)
+        eventos = [(row[0], row[1], row[2]) for row in eventos]
+
+    return eventos
