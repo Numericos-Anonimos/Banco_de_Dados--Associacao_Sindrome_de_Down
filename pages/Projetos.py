951b68e6deccb9ebcb374d5f712fb9bac680133b
Th0m4sma <thomaspirescorreia@gmail.com>
2025-02-19 21:12:10 -0300

    adicionando_projetos

--- pages/Projetos.py
+++ pages/Projetos.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 
 st.set_page_config(page_title="Projetos", layout="wide")
+
+
+st.title("Projetos")
+
+
+with st.sidebar:
+    #st.logo("Imagens/BannerASIN.png", icon_image="Imagens/LogoASIN.png")
+    st.image("Imagens/BannerASIN.png", use_container_width=True)
+    st.write("Ferramentas: ")
